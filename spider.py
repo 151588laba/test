a543daddcea1577e424aba44c209101040480326
151588laba <1868908386@qq.com>
2020-03-24 23:19:43 +0800

    版本2

--- spider.py
+++ spider.py
@@ -2,7 +2,7 @@ import requests
 def main():
     url=input("输入网址链接:")
     UserAgent={"User-Agent":"Mozilla/5.0 (Linux; Android 9; PCAM10 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.110 Mobile Safari/537.36"}
-    req=requests.get(utl=url,headers=UserAgent)
+    req=requests.get(url=url,headers=UserAgent)
     req.encoding="utf-8"
     print(req.text)
 if __name__=="__main__":
